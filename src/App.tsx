46d090c9ea536870c0f6a6a421d357d635028b9d
peetrea <boss.tankionline01@gmail.com>
2022-04-05 23:27:17 +0300

    create lab3

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css';
+
+
+import { LayoutCos } from './Layouts/LayoutCos'
+
+function App() {
+  return (
+    <LayoutCos />
+  );
+}
+
+export default App;
