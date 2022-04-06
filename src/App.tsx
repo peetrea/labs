87159a3cdc29e2f14e51c6b6ed3d3cb382b3ce98
peetrea <boss.tankionline01@gmail.com>
2022-04-06 10:46:47 +0300

    create buttons

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,12 @@ function App() {
     <div>
       <LayoutCos />
       <input value={input1} onChange={(e) => {setInput1(e.target.value)}} placeholder="Input 1" type="text" id="fname"></input>
-      <input value={input2} onChange={(e) => {setInput1(e.target.value)}} placeholder="Input 2"></input>
-      <input value={input3} onChange={(e) => {setInput1(e.target.value)}} placeholder="Input 3"></input>
-      {/* <input type="button" value="Enter" id="fsubmit"></input> */}
+      <input value={input2} onChange={(e) => {setInput2(e.target.value)}} placeholder="Input 2"></input>
+      <input value={input3} onChange={(e) => {setInput3(e.target.value)}} placeholder="Input 3"></input>
+      <p> {input1} </p>
+      <button onClick={()=>alert(input1)}>Button 1</button>
+      <button onClick={()=>alert(input2)}>Button 2</button>
+      <button onClick={()=>alert(input3)}>Button 3</button>
     </div>
   );
 }
